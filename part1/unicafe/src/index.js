7506e934a08500d1fefafc04aa181c7f1036a1b9
root <gsshaurya@gmail.com>
2020-03-27 04:10:05 +0530

    part 1.10 completed

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,11 +2,36 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+
+
+const Statistic = (props) => {
+  return(
+      <p>{props.text} {props.value}</p>
+  )
+}
+
+const Statistics = (props) => {
+  return(
+    <div>
+      <Statistic text={'good'} value={props.good} />
+      <Statistic text={'neutral'} value={props.neutral} />
+      <Statistic text={'bad'} value={props.bad} />
+      <Statistic text={'all'} value={props.all} />
+      <Statistic text={'average'} value={props.average} />
+      <Statistic text={'positive'} value={props.positive} />
+    </div>
+  )
+}
+
+
+
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(1)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [stats, toggleStats] = useState(false)
 
 
 
@@ -15,22 +40,29 @@ const App = () => {
       <h1> Give Feedback</h1>
 
       <div className="feedbackbuttons">
-      <button onClick={() => setGood(good+1)} >Good</button>
-      <button onClick = {() => setNeutral(neutral+1)} >Neutral</button>
-      <button onClick = {() => setBad(bad+1)} >Bad</button>
+      <button onClick={() => {
+        setGood(good+1)
+        toggleStats(true)
+      }} >Good</button>
+      <button onClick = {() => {
+        setNeutral(neutral+1)
+        toggleStats(true)
+      }} >Neutral</button>
+      <button onClick = {() => {
+        setBad(bad+1)
+        toggleStats(true)
+      }} >Bad</button>
       </div>
 
       <h2>Statistics</h2>
 
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good+neutral+bad}</p>
-      <p>average {(good - bad)/(good+bad+neutral)} </p>
-      <p>positive {good/(good+bad+neutral)} </p>
+      { stats ? <Statistics good={good} neutral={neutral} bad={bad} all={good+neutral+bad} average={(good - bad)/(good+bad+neutral)} positive={good/(good+bad+neutral)} />: <p>Submit feedback first</p> 
+}
+
     </div>
   )
 }
+
 
 ReactDOM.render(<App />, 
   document.getElementById('root'))
